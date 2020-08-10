5531ac7004fc2868fe3a5670181697ce9863dcb6
gustavofabro <gustavofabro.f@gmail.com>
2020-08-10 20:32:34 -0300

    Image crop function

--- renderer.js
+++ renderer.js
@@ -1,74 +1,90 @@
-const { desktopCapturer } = require('electron');
-const base64ToImage = require('base64-to-image');
+const { desktopCapturer } = require('electron')
+const base64ToImage = require('base64-to-image')
+const Jimp = require("jimp")
 
 function handleScreenshotToText() {
-  return getScreenshotBase54()
+  return getScreenshotBase64()
+    .then(cropImage)
     .then(saveBase64ToImageFile)
 }
 
-function getScreenshotBase54() {
-  return new Promise((resolve, reject) => {
-    const imageFormat = 'image/png';
+async function getScreenshotBase64() {
+  function getBase64FromStream(stream) {
+    return new Promise((resolve, reject) => {
+      const video = document.createElement('video')
+      const imageFormat = 'image/png'
 
-    function handleStream(stream) {
-      const video = document.createElement('video');
-
-      video.style.cssText = 'position:absolute;top:-10000px;left:-10000px;';
+      video.style.cssText = 'position:absolutetop:-10000pxleft:-10000px'
 
       video.onloadedmetadata = function () {
-        video.style.height = this.videoHeight + 'px';
-        video.style.width = this.videoWidth + 'px';
+        video.style.height = this.videoHeight + 'px'
+        video.style.width = this.videoWidth + 'px'
 
-        video.play();
+        video.play()
 
-        const canvas = document.createElement('canvas');
-        canvas.width = this.videoWidth;
-        canvas.height = this.videoHeight;
-        const ctx = canvas.getContext('2d');
+        const canvas = document.createElement('canvas')
+        canvas.width = this.videoWidth
+        canvas.height = this.videoHeight
 
-        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-        resolve(canvas.toDataURL(imageFormat))
-        video.remove();
+        const ctx = canvas.getContext('2d')
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+
+        video.remove()
 
         try {
-          stream.getTracks()[0].stop();
-        } catch (e) { }
+          stream.getTracks()[0].stop()
+        } finally {
+          resolve(canvas.toDataURL(imageFormat))
+        }
       }
 
-      video.srcObject = stream;
-      document.body.appendChild(video);
-    };
+      video.srcObject = stream
+      document.body.appendChild(video)
+    })
+  }
 
-    desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async sources => {
-      const sourceEntireScreen = sources
-        .find(source => source.name == 'Entire Screen')
+  const sources = await desktopCapturer.getSources({ types: ['window', 'screen'] })
+  const sourceEntireScreen = sources.find(source => source.name == 'Entire Screen')
 
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          audio: false,
-          video: {
-            mandatory: {
-              chromeMediaSource: 'desktop',
-              chromeMediaSourceId: sourceEntireScreen.id,
-              minWidth: 1280,
-              maxWidth: 4000,
-              minHeight: 720,
-              maxHeight: 4000
-            }
-          }
-        });
-        handleStream(stream);
-      } catch (e) {
-        reject(e)
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        chromeMediaSourceId: sourceEntireScreen.id,
+        minWidth: 1280,
+        maxWidth: 4000,
+        minHeight: 720,
+        maxHeight: 4000
       }
-    });
+    }
   })
+
+  return getBase64FromStream(stream)
+}
+
+async function cropImage(base64data) {
+  const encondedImageBuffer = new Buffer(base64data.replace(/^data:image\/(png|gif|jpeg);base64,/, ''), 'base64')
+  const height = window.innerHeight
+  const width = window.innerWidth
+  const distanceX = window.screenLeft
+  const distanceY = window.screenTop
+  const screenHeight = screen.height
+  const screenWidth = screen.width
+
+  const image = await Jimp.read(encondedImageBuffer)
+  image.resize(screenWidth, screenHeight)
+
+  const cropedImage = await image.crop(distanceX, distanceY, width, height)
+
+  return cropedImage.getBase64Async('image/png')
 }
 
 function saveBase64ToImageFile(base64Str) {
-  const path = './';
-  const options = { fileName: `file-${Date.now()}`, type: 'png' };
+  const path = './'
+  const options = { fileName: `file-${Date.now()}`, type: 'png' }
 
-  const { imageType, fileName } = base64ToImage(base64Str, path, options);
+  const { imageType, fileName } = base64ToImage(base64Str, path, options)
 
+  console.log(imageType, fileName)
 }
