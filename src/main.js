c36a5799e644c01055de8730245a67b184dd6ba8
gustavofabro <gustavofabro.f@gmail.com>
2020-08-15 14:43:02 -0300

    full screen mode

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 const { app, BrowserWindow } = require('electron')
 
-app.whenReady().then(() => {
+function onReady() {
   const win = new BrowserWindow({
-    width: 800,
+    width: 900,
     height: 600,
+    titleBarStyle: 'hidden',
+    show: false,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    center: true,
+    transparent: true,
+    frame: false,
+    alwaysOnTop: true
   })
 
+  win.maximize()
+  win.show()
+
   win.loadFile('index.html')
-})
+}
+
+app.whenReady().then(() => setTimeout(onReady, 100))
